a8e64fbb65f590fd3280536d7b8296dde192133e
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-11-15 12:35:00 -0600

    finished

--- cpu.py
+++ cpu.py
@@ -11,17 +11,22 @@ class CPU:
         self.pc = 0
         self.ram = [0] * 256
         self.reg = [0] * 8
+        self.flag = [0] * 8
         self.sp = 7
         self.instructions = {
-            1: self.HLT,
-            130: self.LDI,
-            71: self.PRN,
-            69: self.PUSH,
-            70: self.POP,
-            80: self.CALL,
-            17: self.RET,
-            160: self.ADD,
-            162: self.MUL
+            1: self.hlt,
+            130: self.ldi,
+            71: self.prn,
+            69: self.push,
+            70: self.pop,
+            80: self.call,
+            17: self.ret,
+            160: self.add,
+            162: self.mul,
+            167: self.cmp,
+            84: self.jmp,
+            85: self.jeq,
+            86: self.jne
         }
 
     def load(self, path):
@@ -41,17 +46,29 @@ class CPU:
             self.ram[address] = instruction
             address += 1
 
+    def ram_read(self, mar):
+        return self.ram[mar]
+
+    def ram_write(self, value, mar):
+        self.ram[mar] = value
+
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
+        val_one = self.reg[reg_a]
+        val_two = self.reg[reg_b]
 
-        if op == "ADD":
+        if op == "add":
             self.reg[reg_a] += self.reg[reg_b]
-        elif op == "MUL":
+        elif op == "mul":
             self.reg[reg_a] *= self.reg[reg_b]
-        elif op == "SUB":
-            self.reg[reg_a] -= self.reg[reg_b]
-        elif op == "DIV":
-            self.reg[reg_a] /= self.reg[reg_b]
+        elif op == "cmp":
+            if val_one == val_two:
+                self.flag[7] = 1
+            if val_one > val_two:
+                self.flag[6] = 1
+            if val_one < val_two:
+                self.flag[5] = 1
+
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -63,7 +80,7 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            # self.fl,
+            # self.flag,
             # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
@@ -75,67 +92,84 @@ class CPU:
 
         print()
 
-    def HLT(self):
+    def hlt(self):
         sys.exit(1)
         self.running = False
 
-    def LDI(self):
+    def ldi(self):
         reg_a = self.ram_read(self.pc + 1)
         num = self.ram_read(self.pc + 2)
         self.reg[reg_a] = num
-        self.pc +=3
+        self.pc += 3
 
-    def PRN(self):
+    def prn(self):
         print(self.reg[self.ram_read(self.pc + 1)])
         self.pc += 2
 
-    def PUSH(self):
+    def push(self):
         reg_a = self.ram[self.pc + 1]
         value = self.reg[reg_a]
-        self.reg[self.sp] -= 1 # decrement the pointer address
+        self.reg[self.sp] -= 1
         self.ram[self.reg[self.sp]] = value
         self.pc += 2
 
-    def POP(self):
+    def pop(self):
         reg_a = self.ram[self.pc + 1]
         value = self.ram[self.reg[self.sp]]
         self.reg[reg_a] = value
         self.reg[self.sp] += 1
         self.pc += 2
 
-    def CALL(self):
+    def call(self):
         self.reg[self.sp] -= 1
         self.ram[self.reg[self.sp]] = self.pc + 2
         reg_a = self.ram[self.pc + 1]
         self.reg[6] = self.pc + 2
         self.pc = self.reg[reg_a]
 
-    def RET(self):
-        # pc = self.ram[self.reg[self.sp]]
+    def ret(self):
         self.reg[self.sp] += 1
         self.pc = self.reg[6]
-    
-    def ADD(self):
+
+    def add(self):
         reg_a = self.ram_read(self.pc + 1)
         reg_b = self.ram_read(self.pc + 2)
-        self.alu('ADD', reg_a, reg_b)
-        self.pc +=3
-    
-    def MUL(self):
-        reg_a = self.ram_read(self.pc + 1)
-        reg_b = self.ram_read(self.pc + 2)
-        self.alu('MUL', reg_a, reg_b)
+        self.alu('add', reg_a, reg_b)
         self.pc += 3
+
+    def mul(self):
+        reg_a = self.ram_read(self.pc + 1)
+        reg_b = self.ram_read(self.pc + 2)
+        self.alu('mul', reg_a, reg_b)
+        self.pc += 3
+
+    def cmp(self):
+        reg_a = self.ram[self.pc + 1]
+        reg_b = self.ram[self.pc + 2]
+        self.alu('cmp', reg_a, reg_b)
+        self.pc += 3
+
+    def jmp(self):
+        reg_a = self.ram[self.pc + 1]
+        self.pc = self.reg[reg_a]
+
+    def jeq(self):
+        if self.flag[7] == 1:
+            reg_a = self.ram[self.pc + 1]
+            self.pc = self.reg[reg_a]
+        else:
+            self.pc += 2
+
+    def jne(self):
+        if self.flag[7] == 0:
+            reg_a = self.ram[self.pc + 1]
+            self.pc = self.reg[reg_a]
+        else:
+            self.pc += 2
 
     def run(self):
         """Run the CPU."""
 
-        while True: 
+        while True:
             register = self.ram_read(self.pc)
             self.instructions[register]()
-
-    def ram_read(self, mar):
-        return self.ram[mar]
-
-    def ram_write(self, value, mar):
-        self.ram[mar] = value
